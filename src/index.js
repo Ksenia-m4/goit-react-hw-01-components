761240c599ac073c569876c04dbaf0702d402a93
Kseniia Machnieva <164485727+Ksenia-m4@users.noreply.github.com>
2024-09-05 11:29:01 +0200

    updated template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-// const element = React.createElement("div");
-// console.log(element);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
